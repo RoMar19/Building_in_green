a074d1585592a3ce4f275d837893368fdc6a49fc
Romart <rmf1312@hotmail.com>
2020-06-04 12:45:12 +0000

    all_gallery.html deleted, replaced by gallery.html

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGODB_NAME"] = 'green_buildings'
-app.config["MONGO_URI"] = 'mongodb+srv://RoMar19:CodeStudent@cluster0-oourq.mongodb.net/green-buildings?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://RoMar19:CodeStudent@cluster0-oourq.mongodb.net/green_buildings?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -17,38 +17,40 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html", page_title="Home")
+    categories = mongo.db.categories.find()
+    return render_template("index.html", page_title="Home", categories=categories)
 
-# Shows all houses of DB
-@app.route('/all_gallery')
-def all_gallery():
-    houses = mongo.db.houses.find()
-    return render_template ("all_gallery.html", houses=houses)
+
 
 # Shows all houses in the gallery(DB) by category selection
-@app.route('/gallery/<select_category>')
-def gallery(select_category):
-    selected_gallery = mongo.db.houses.find()
+@app.route('/gallery/<selected_category>')
+def gallery(selected_category):
+    if selected_category == "all":
+
+        houses = mongo.db.houses.find()
+    else: 
+        houses = mongo.db.houses.find(
+            { "category_name": selected_category }
+        )
     return render_template("gallery.html", 
-                          houses=selected_gallery,
-                          select_category=select_category,
-                          page_title=select_category + "Green Buildings")
+                          houses=houses,
+                          page_title=selected_category.capitalize() + "Green Buildings")
 
 # Shows details of the selected house with butons to Edit or Delete 
 @app.route('/house_detail/<house_id>')
 def house_detail(house_id):
-    house_details = mongo.db.houses.find_one({"_id": ObjectId(house_id)})
+    house = mongo.db.houses.find_one({"_id": ObjectId(house_id)})
     return render_template("house_detail.html",
-                          houses=house_details,
+                          house=house,
                           page_title="Green building Info.")                          
                      
 
 # Shows form page to add new houses of the users to the Gallery of the Comunity(DB) 
 @app.route('/add_house')
 def add_house():
-    house_categories = mongo.db.categories.find()
+    categories = mongo.db.categories.find()
     return render_template("add_house.html", 
-                        categories=house_categories,
+                        categories=categories,
                         page_title="Add Your Own Green Building")
 
 # Inserts new houses to the DB after fill in Form add_house and press button
